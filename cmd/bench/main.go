9452f699bfc1d02270a45b3834fe0e7c0b01d860
Dumi Loghin <dumiloghin@gmail.com>
2018-11-20 14:44:12 +0800

    run 256 and 512 separately

--- cmd/bench/main.go
+++ cmd/bench/main.go
@@ -5,23 +5,27 @@ import (
 	"os"
 	"strconv"
 	"time"
-	
+
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
 func main() {
-	var size = 1000000	
-	if len(os.Args) > 1 {		
-		n, err := strconv.Atoi(os.Args[1])
+	var size = 1000000
+	var bench = "Keccak256"
+	if len(os.Args) > 2 {
+		bench = os.Args[1]
+		n, err := strconv.Atoi(os.Args[2])
 		if err != nil {
-			fmt.Println("Invalid size", os.Args[1])
+			fmt.Println("Invalid size", os.Args[2])
 			return
 		}
 		size = n
+	} else {
+		fmt.Println("Usage:",os.Args[0]," <Keccak256|Keccak512> <size>")
 	}
-	
-	fmt.Println("Running Keccak256 and Keccak512 on input of size", size)
-	
+
+	fmt.Println("Running", bench, "on input of size", size)
+
 	start := time.Now()
 	data := make([]byte, size)
 	for i := 0; i < size; i++ {
@@ -30,18 +34,22 @@ func main() {
 	t := time.Now()
 	elapsed := t.Sub(start)
 	fmt.Println("Array time", elapsed)
-	
-	start = time.Now()
-	hash := crypto.Keccak256(data)
-	t = time.Now()
-	elapsed = t.Sub(start)
-	fmt.Println("Keccak256 time", elapsed)
-	fmt.Println(hash)
-	
-	start = time.Now()
-	hash = crypto.Keccak512(data)
-	t = time.Now()
-	elapsed = t.Sub(start)
-	fmt.Println("Keccak512 time", elapsed)
-	fmt.Println(hash)
+
+	if bench == "Keccak256" {
+		start = time.Now()
+		hash := crypto.Keccak256(data)
+		t = time.Now()
+		elapsed = t.Sub(start)
+		fmt.Println("Keccak256 time", elapsed)
+		fmt.Println(hash)
+	} else if bench == "Keccak512" {
+		start = time.Now()
+		hash := crypto.Keccak512(data)
+		t = time.Now()
+		elapsed = t.Sub(start)
+		fmt.Println("Keccak512 time", elapsed)
+		fmt.Println(hash)
+	} else {
+		fmt.Println("Invalid bench", bench)
+	}
 }
